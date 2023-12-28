0e8058b28c55ce0e633556a02ff223845e0c74df
bohecola <bohecola@outlook.com>
2023-12-28 23:55:07 +0800

    build: split worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from "path"
 
+const prefix = `monaco-editor/esm/vs`
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          jsonWorker: [`${prefix}/language/json/json.worker`],
+          cssWorker: [`${prefix}/language/css/css.worker`],
+          htmlWorker: [`${prefix}/language/html/html.worker`],
+          tsWorker: [`${prefix}/language/typescript/ts.worker`],
+          editorWorker: [`${prefix}/editor/editor.worker`],
+        }
+      }
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
